c0c3bec633107fb66db98c537e0192ed59a3f9e6
Isaias Villalobos <isaiasvillalobos98@gmail.com>
2020-12-14 17:04:46 -0500

    Added more components to the project to organize the currency converter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ConverterComponent } from './converter/converter.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ConverterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
